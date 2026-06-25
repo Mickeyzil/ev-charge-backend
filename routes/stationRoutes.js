ba30c77777f94a44f3effc23b677f09ab15884df
gilcohen23 <gilcohen080@gmail.com>
2026-06-25 20:35:00 +0300

    fix

--- routes/stationRoutes.js
+++ routes/stationRoutes.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { getAllStations, updateStationByExternalCompany } = require('../controllers/stationController');
 
-// נתיב לקבלת כל התחנות
-router.get('/external-update/:station_id', getAllStations);
+router.get('/', getAllStations);
 router.put('/external-update/:station_id', updateStationByExternalCompany);
-
 module.exports = router;
